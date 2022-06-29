6f02d16618542b6e9a1eeedb15668d4fca1ade3d
zionexeonch <zionexeon@gmail.com>
2022-06-29 22:11:35 +0700

    edit code

--- resources/views/home/portfolio.blade.php
+++ resources/views/home/portfolio.blade.php
@@ -5,125 +5,171 @@
     <div class="background-portfolio">
         <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
             <li class="nav-item" role="presentation">
-                <button class="nav-link active button" id="pills-all-tab" data-bs-toggle="pill" data-bs-target="#pills-all" type="button" role="tab" aria-controls="pills-all" aria-selected="true">All</button>
+                <button class="nav-link active button" id="pills-all-tab" data-bs-toggle="pill"
+                    data-bs-target="#pills-all" type="button" role="tab" aria-controls="pills-all"
+                    aria-selected="true">All</button>
             </li>
             <li class="nav-item" role="presentation">
-                <button class="nav-link button" id="pills-timbul-tab" data-bs-toggle="pill" data-bs-target="#pills-timbul" type="button" role="tab" aria-controls="pills-timbul" aria-selected="false">Huruf Timbul</button>
+                <button class="nav-link button" id="pills-timbul-tab" data-bs-toggle="pill"
+                    data-bs-target="#pills-timbul" type="button" role="tab" aria-controls="pills-timbul"
+                    aria-selected="false">Huruf Timbul</button>
             </li>
             <li class="nav-item" role="presentation">
-                <button class="nav-link button" id="pills-neonbox-tab" data-bs-toggle="pill" data-bs-target="#pills-neonbox" type="button" role="tab" aria-controls="pills-neonbox" aria-selected="false">Neonbox</button>
+                <button class="nav-link button" id="pills-neonbox-tab" data-bs-toggle="pill"
+                    data-bs-target="#pills-neonbox" type="button" role="tab" aria-controls="pills-neonbox"
+                    aria-selected="false">Neonbox</button>
             </li>
             <li class="nav-item" role="presentation">
-                <button class="nav-link button" id="pills-reklame-tab" data-bs-toggle="pill" data-bs-target="#pills-reklame" type="button" role="tab" aria-controls="pills-reklame" aria-selected="false">Project
+                <button class="nav-link button" id="pills-reklame-tab" data-bs-toggle="pill"
+                    data-bs-target="#pills-reklame" type="button" role="tab" aria-controls="pills-reklame"
+                    aria-selected="false">Project
                     Reklame</button>
             </li>
             <li class="nav-item" role="presentation">
-                <button class="nav-link button" id="pills-digitalcreative-tab" data-bs-toggle="pill" data-bs-target="#pills-digitalcreative" type="button" role="tab" aria-controls="pills-digitalcreative" aria-selected="false">Digital Creative</button>
+                <button class="nav-link button" id="pills-digitalcreative-tab" data-bs-toggle="pill"
+                    data-bs-target="#pills-digitalcreative" type="button" role="tab"
+                    aria-controls="pills-digitalcreative" aria-selected="false">Digital Creative</button>
             </li>
             <li class="nav-item" role="presentation">
-                <button class="nav-link button" id="pills-iot-tab" data-bs-toggle="pill" data-bs-target="#pills-iot" type="button" role="tab" aria-controls="pills-iot" aria-selected="false">Prototype IOT</button>
+                <button class="nav-link button" id="pills-iot-tab" data-bs-toggle="pill" data-bs-target="#pills-iot"
+                    type="button" role="tab" aria-controls="pills-iot" aria-selected="false">Prototype IOT</button>
             </li>
         </ul>
         <div class="tab-content" id="pills-tabContent">
-            <div class="tab-pane fade show active" id="pills-all" role="tabpanel" aria-labelledby="pills-all-tab" tabindex="0">
+            <div class="tab-pane fade show active" id="pills-all" role="tabpanel" aria-labelledby="pills-all-tab"
+                tabindex="0">
                 <div class="row row-cols-1 row-cols-md-3 g-4">
                     @foreach ($portfolio as $portfolio)
-                        <div class="col">
-                            <div class="card bg-black overlay card-hover1 border-0" data-aos="fade-down" data-aos-duration="1000">
-                                <a href="/portfolio/<?= $portfolio->slug ?>" class="text-white">
-                                    <img src="/img/portfolio/<?= $portfolio->gambar ?>" id="gambar" class="card-img" alt="Portfolio">
-                                    <div class="overlay-content card-img-overlay text-bottom">
-                                        <h5><?= $portfolio->judul_produk ?></h5>
-                                        <span class="text-uppercase"><?= $portfolio->nama_kategori ?><span>                                           
-                                    </div>
-                                </a>
-                            </div>
+                    <div class="col">
+                        <div class="card bg-black overlay card-hover1 border-0" data-aos="fade-down"
+                            data-aos-duration="1000">
+                            <a href="/portfolio/<?= $portfolio->slug ?>" class="text-white">
+                                <img src="/img/portfolio/<?= $portfolio->gambar ?>" id="gambar" class="card-img"
+                                    alt="Portfolio">
+                                <div class="overlay-content card-img-overlay text-bottom">
+                                    <h5>
+                                        <?= $portfolio->judul_produk ?>
+                                    </h5>
+                                    <span class="text-uppercase">
+                                        <?= $portfolio->nama_kategori ?><span>
+                                </div>
+                            </a>
                         </div>
+                    </div>
                     @endforeach
                 </div>
             </div>
-            <div class="tab-pane fade" id="pills-timbul" role="tabpanel" aria-labelledby="pills-timbul-tab" tabindex="0">
+            <div class="tab-pane fade" id="pills-timbul" role="tabpanel" aria-labelledby="pills-timbul-tab"
+                tabindex="0">
                 <div class="row row-cols-1 row-cols-md-3 g-4">
                     @foreach ($huruftimbul as $huruftimbul)
-                        <div class="col">
-                            <div class="card bg-black overlay card-hover1 border-0" data-aos="fade-down" data-aos-duration="1000">
-                                <a href="/portfolio/<?= $huruftimbul->slug ?>" class="text-white">
-                                    <img src="/img/portfolio/<?= $huruftimbul->gambar ?>" id="gambar" class="card-img" alt="Portfolio">
-                                    <div class="overlay-content card-img-overlay text-bottom">
-                                        <h5><?= $huruftimbul->judul_produk ?></h5>
-                                        <span class="text-uppercase"><?= $huruftimbul->nama_kategori ?><span>                                           
-                                    </div>
-                                </a>
-                            </div>
+                    <div class="col">
+                        <div class="card bg-black overlay card-hover1 border-0" data-aos="fade-down"
+                            data-aos-duration="1000">
+                            <a href="/portfolio/<?= $huruftimbul->slug ?>" class="text-white">
+                                <img src="/img/portfolio/<?= $huruftimbul->gambar ?>" id="gambar" class="card-img"
+                                    alt="Portfolio">
+                                <div class="overlay-content card-img-overlay text-bottom">
+                                    <h5>
+                                        <?= $huruftimbul->judul_produk ?>
+                                    </h5>
+                                    <span class="text-uppercase">
+                                        <?= $huruftimbul->nama_kategori ?><span>
+                                </div>
+                            </a>
                         </div>
+                    </div>
                     @endforeach
                 </div>
             </div>
-            <div class="tab-pane fade" id="pills-neonbox" role="tabpanel" aria-labelledby="pills-neonbox-tab" tabindex="0">
+            <div class="tab-pane fade" id="pills-neonbox" role="tabpanel" aria-labelledby="pills-neonbox-tab"
+                tabindex="0">
                 <div class="row row-cols-1 row-cols-md-3 g-4">
-                	@foreach ($neon as $neon)
-                        <div class="col">
-                            <div class="card bg-black overlay card-hover1 border-0" data-aos="fade-down" data-aos-duration="1000">
-                                <a href="/portfolio/<?= $neon->slug ?>" class="text-white">
-                                    <img src="/img/portfolio/<?= $neon->gambar ?>" id="gambar" class="card-img" alt="Portfolio">
-                                    <div class="overlay-content card-img-overlay text-bottom">
-                                        <h5><?= $neon->judul_produk ?></h5>
-                                        <span class="text-uppercase"><?= $neon->nama_kategori ?><span>                                           
-                                    </div>
-                                </a>
-                            </div>
+                    @foreach ($neon as $neon)
+                    <div class="col">
+                        <div class="card bg-black overlay card-hover1 border-0" data-aos="fade-down"
+                            data-aos-duration="1000">
+                            <a href="/portfolio/<?= $neon->slug ?>" class="text-white">
+                                <img src="/img/portfolio/<?= $neon->gambar ?>" id="gambar" class="card-img"
+                                    alt="Portfolio">
+                                <div class="overlay-content card-img-overlay text-bottom">
+                                    <h5>
+                                        <?= $neon->judul_produk ?>
+                                    </h5>
+                                    <span class="text-uppercase">
+                                        <?= $neon->nama_kategori ?><span>
+                                </div>
+                            </a>
                         </div>
-                    @endforeach                                       
+                    </div>
+                    @endforeach
                 </div>
             </div>
-            <div class="tab-pane fade" id="pills-reklame" role="tabpanel" aria-labelledby="pills-reklame-tab" tabindex="0">
+            <div class="tab-pane fade" id="pills-reklame" role="tabpanel" aria-labelledby="pills-reklame-tab"
+                tabindex="0">
                 <div class="row row-cols-1 row-cols-md-3 g-4">
                     @foreach ($reklame as $reklame)
-                        <div class="col">
-                            <div class="card bg-black overlay card-hover1 border-0" data-aos="fade-down" data-aos-duration="1000">
-                                <a href="/portfolio/<?= $reklame->slug ?>" class="text-white">
-                                    <img src="/img/portfolio/<?= $reklame->gambar ?>" id="gambar" class="card-img" alt="Portfolio">
-                                    <div class="overlay-content card-img-overlay text-bottom">
-                                        <h5><?= $reklame->judul_produk ?></h5>
-                                        <span class="text-uppercase"><?= $reklame->nama_kategori ?><span>                                           
-                                    </div>
-                                </a>
-                            </div>
+                    <div class="col">
+                        <div class="card bg-black overlay card-hover1 border-0" data-aos="fade-down"
+                            data-aos-duration="1000">
+                            <a href="/portfolio/<?= $reklame->slug ?>" class="text-white">
+                                <img src="/img/portfolio/<?= $reklame->gambar ?>" id="gambar" class="card-img"
+                                    alt="Portfolio">
+                                <div class="overlay-content card-img-overlay text-bottom">
+                                    <h5>
+                                        <?= $reklame->judul_produk ?>
+                                    </h5>
+                                    <span class="text-uppercase">
+                                        <?= $reklame->nama_kategori ?><span>
+                                </div>
+                            </a>
                         </div>
+                    </div>
                     @endforeach
                 </div>
             </div>
-            <div class="tab-pane fade" id="pills-digitalcreative" role="tabpanel" aria-labelledby="pills-digitalcreative-tab" tabindex="0">
+            <div class="tab-pane fade" id="pills-digitalcreative" role="tabpanel"
+                aria-labelledby="pills-digitalcreative-tab" tabindex="0">
                 <div class="row row-cols-1 row-cols-md-3 g-4">
                     @foreach ($digital as $digital)
-                        <div class="col">
-                            <div class="card bg-black overlay card-hover1 border-0" data-aos="fade-down" data-aos-duration="1000">
-                                <a href="/portfolio/<?= $digital->slug ?>" class="text-white">
-                                    <img src="/img/portfolio/<?= $digital->gambar ?>" id="gambar" class="card-img" alt="Portfolio">
-                                    <div class="overlay-content card-img-overlay text-bottom">
-                                        <h5><?= $digital->judul_produk ?></h5>
-                                        <span class="text-uppercase"><?= $digital->nama_kategori ?><span>                                           
-                                    </div>
-                                </a>
-                            </div>
+                    <div class="col">
+                        <div class="card bg-black overlay card-hover1 border-0" data-aos="fade-down"
+                            data-aos-duration="1000">
+                            <a href="/portfolio/<?= $digital->slug ?>" class="text-white">
+                                <img src="/img/portfolio/<?= $digital->gambar ?>" id="gambar" class="card-img"
+                                    alt="Portfolio">
+                                <div class="overlay-content card-img-overlay text-bottom">
+                                    <h5>
+                                        <?= $digital->judul_produk ?>
+                                    </h5>
+                                    <span class="text-uppercase">
+                                        <?= $digital->nama_kategori ?><span>
+                                </div>
+                            </a>
                         </div>
+                    </div>
                     @endforeach
                 </div>
             </div>
             <div class="tab-pane fade" id="pills-iot" role="tabpanel" aria-labelledby="pills-iot-tab" tabindex="0">
                 <div class="row row-cols-1 row-cols-md-3 g-4">
                     @foreach ($iot as $iot)
-                        <div class="col">
-                            <div class="card bg-black overlay card-hover1 border-0" data-aos="fade-down" data-aos-duration="1000">
-                                <a href="/portfolio/<?= $iot->slug ?>" class="text-white">
-                                    <img src="/img/portfolio/<?= $iot->gambar ?>" id="gambar" class="card-img" alt="Portfolio">
-                                    <div class="overlay-content card-img-overlay text-bottom">
-                                        <h5><?= $iot->judul_produk ?></h5>
-                                        <span class="text-uppercase"><?= $iot->nama_kategori ?><span>                                           
-                                    </div>
-                                </a>
-                            </div>
+                    <div class="col">
+                        <div class="card bg-black overlay card-hover1 border-0" data-aos="fade-down"
+                            data-aos-duration="1000">
+                            <a href="/portfolio/<?= $iot->slug ?>" class="text-white">
+                                <img src="/img/portfolio/<?= $iot->gambar ?>" id="gambar" class="card-img"
+                                    alt="Portfolio">
+                                <div class="overlay-content card-img-overlay text-bottom">
+                                    <h5>
+                                        <?= $iot->judul_produk ?>
+                                    </h5>
+                                    <span class="text-uppercase">
+                                        <?= $iot->nama_kategori ?><span>
+                                </div>
+                            </a>
                         </div>
+                    </div>
                     @endforeach
                 </div>
             </div>
--- resources/views/home/product.blade.php
+++ resources/views/home/product.blade.php
@@ -18,29 +18,41 @@
             <article>
                 <h3 class="fw-bold">Informasi Produk</h3>
                 <p>
-                    Lembaran akrilik dari Akrilik Laser Cutting dibuat dari 100% MMA murni (metil metrakrilat monomer) yang menjamin
-                    produk akrilik yang dihasilkan memiliki kualitas tertinggi. Bahan ini juga memastikan tingkat
-                    kejernihan yang sangat baik, ketahanan terhadap cuaca dan kekuatan tinggi. Laboratorium Penjamin
-                    Pelaksana (Underwriters) telah memberikan produk sertifikasi UL-94HB, yang merupakan ambang bakar
-                    tertinggi untuk lembaran akrilik dengan transmisi cahaya 92%. Produk ini dapat dibentuk dalam
-                    temperatur tinggi (thermoformed), dipotong, dibor, dilengkungkan, diolah dengan mesin CNC, diukir,
+                    Lembaran akrilik dari Akrilik Laser Cutting dibuat dari 100%
+                    MMA murni (metil metrakrilat monomer) yang menjamin
+                    produk akrilik yang dihasilkan memiliki kualitas tertinggi.
+                    Bahan ini juga memastikan tingkat
+                    kejernihan yang sangat baik, ketahanan terhadap cuaca dan
+                    kekuatan tinggi. Laboratorium Penjamin
+                    Pelaksana (Underwriters) telah memberikan produk sertifikasi
+                    UL-94HB, yang merupakan ambang bakar
+                    tertinggi untuk lembaran akrilik dengan transmisi cahaya
+                    92%. Produk ini dapat dibentuk dalam
+                    temperatur tinggi (thermoformed), dipotong, dibor,
+                    dilengkungkan, diolah dengan mesin CNC, diukir,
                     diasah dan dilem.
                 </p>
                 <p>
-                    Kualitas ASTARIGLAS® GP mengikuti standar-standar kualitas yang ada di dunia yang menuntut kualitas
-                    tinggi, memberikan karakteristik optis yang sangat baik, stabilitas cahaya dan tingkat tekanan
+                    Kualitas ASTARIGLAS® GP mengikuti standar-standar kualitas
+                    yang ada di dunia yang menuntut kualitas
+                    tinggi, memberikan karakteristik optis yang sangat baik,
+                    stabilitas cahaya dan tingkat tekanan
                     internal yang rendah untuk performa yang konsisten.
                 </p>
                 <p>
-                    ASTARIGLAS® GP tersedia dalam berbagai macam standar ukuran dan ketebalan, bening maupun berwarna.
+                    ASTARIGLAS® GP tersedia dalam berbagai macam standar ukuran
+                    dan ketebalan, bening maupun berwarna.
                 </p>
             </article>
             <article>
                 <h3 class="fw-bold">Karakteristik</h3>
                 <p>
-                    ASTARIGLAS® GP adalah bahan termoplastik yang ringan dan kaku, dengan resistensi kerusakan (pecah)
-                    yang lebih tinggi dibandingkan kaca biasa dan sangat tahan kondisi cuaca. ASTARIGLAS® GP dapat
-                    dengan mudah digergaji mesin, dibentuk dalam suhu tinggi dan disemen, serta sedikit menyerap sinar
+                    ASTARIGLAS® GP adalah bahan termoplastik yang ringan dan
+                    kaku, dengan resistensi kerusakan (pecah)
+                    yang lebih tinggi dibandingkan kaca biasa dan sangat tahan
+                    kondisi cuaca. ASTARIGLAS® GP dapat
+                    dengan mudah digergaji mesin, dibentuk dalam suhu tinggi dan
+                    disemen, serta sedikit menyerap sinar
                     ultraviolet.
                 </p>
             </article>
@@ -52,43 +64,66 @@
     <div class="background-produk">
         <ul class="nav nav-pills mb-3 justify-content-center p-3" id="pills-tab" role="tablist">
             <li class="nav-item" role="presentation">
-              <button class="nav-link active" id="pills-bening-tab" data-bs-toggle="pill" data-bs-target="#pills-bening" type="button" role="tab" aria-controls="pills-bening" aria-selected="true">BENING</button>
+                <button class="nav-link active" id="pills-bening-tab" data-bs-toggle="pill"
+                    data-bs-target="#pills-bening" type="button" role="tab" aria-controls="pills-bening"
+                    aria-selected="true">BENING</button>
             </li>
             <li class="nav-item" role="presentation">
-              <button class="nav-link" id="pills-putih-tab" data-bs-toggle="pill" data-bs-target="#pills-putih" type="button" role="tab" aria-controls="pills-putih" aria-selected="false">PUTIH SUSU DAN PUTIH SOLID</button>
+                <button class="nav-link" id="pills-putih-tab" data-bs-toggle="pill" data-bs-target="#pills-putih"
+                    type="button" role="tab" aria-controls="pills-putih" aria-selected="false">PUTIH SUSU DAN PUTIH
+                    SOLID</button>
             </li>
             <li class="nav-item" role="presentation">
-              <button class="nav-link" id="pills-transparan-tab" data-bs-toggle="pill" data-bs-target="#pills-transparan" type="button" role="tab" aria-controls="pills-transaparan" aria-selected="false">WARNA TRANSPARAN</button>
+                <button class="nav-link" id="pills-transparan-tab" data-bs-toggle="pill"
+                    data-bs-target="#pills-transparan" type="button" role="tab" aria-controls="pills-transaparan"
+                    aria-selected="false">WARNA TRANSPARAN</button>
             </li>
             <li class="nav-item" role="presentation">
-              <button class="nav-link" id="pills-tinted-tab" data-bs-toggle="pill" data-bs-target="#pills-tinted" type="button" role="tab" aria-controls="pills-tinted" aria-selected="false">WARNA TINTED</button>
+                <button class="nav-link" id="pills-tinted-tab" data-bs-toggle="pill" data-bs-target="#pills-tinted"
+                    type="button" role="tab" aria-controls="pills-tinted" aria-selected="false">WARNA TINTED</button>
             </li>
             <li class="nav-item" role="presentation">
-              <button class="nav-link" id="pills-wsb-tab" data-bs-toggle="pill" data-bs-target="#pills-wsb" type="button" role="tab" aria-controls="pills-wsb" aria-selected="false">WARNA SOLID DAN BURAM</button>
+                <button class="nav-link" id="pills-wsb-tab" data-bs-toggle="pill" data-bs-target="#pills-wsb"
+                    type="button" role="tab" aria-controls="pills-wsb" aria-selected="false">WARNA SOLID DAN
+                    BURAM</button>
             </li>
             <li class="nav-item" role="presentation">
-              <button class="nav-link" id="pills-neon-tab" data-bs-toggle="pill" data-bs-target="#pills-neon" type="button" role="tab" aria-controls="pills-neon" aria-selected="false">WARNA FLUORESCENT / NEON</button>
+                <button class="nav-link" id="pills-neon-tab" data-bs-toggle="pill" data-bs-target="#pills-neon"
+                    type="button" role="tab" aria-controls="pills-neon" aria-selected="false">WARNA FLUORESCENT /
+                    NEON</button>
             </li>
-          </ul>
-          <div class="tab-content" id="pills-tabContent">
+        </ul>
+        <div class="tab-content" id="pills-tabContent">
             <div class="tab-pane fade show active" id="pills-bening" role="tabpanel" aria-labelledby="pills-bening-tab">
                 <div class="container">
                     <div class="row">
                         <div class="col-md-8">
                             <h4 class="fw-bold">Bening</h4>
                             <article>
-                                <p>Lembaran kilap yang bening, tidak berwarna dan memiliki tingkat transparansi yang tinggi, yang memungkinkan untuk menghantarkan cahaya hingga 92%. Kualitas lain yang juga terdapat pada acrylic kami adalah ketahanan terhadap kondisi cuaca dan ketahanan terhadap keretakan. Menjadikan Astariglas® pilihan yang tepat bagi para pengguna produk acrylic yang membutuhkan bahan/materi tembus pandang, seperti akuarim, peredam suara, dan pembatas keamanan untuk teller di bank.</p>
+                                <p>Lembaran kilap yang bening, tidak berwarna
+                                    dan memiliki tingkat transparansi yang
+                                    tinggi, yang memungkinkan untuk
+                                    menghantarkan cahaya hingga 92%. Kualitas
+                                    lain yang juga terdapat pada acrylic kami
+                                    adalah ketahanan terhadap kondisi cuaca dan
+                                    ketahanan terhadap keretakan. Menjadikan
+                                    Astariglas® pilihan yang tepat bagi para
+                                    pengguna produk acrylic yang membutuhkan
+                                    bahan/materi tembus pandang, seperti
+                                    akuarim, peredam suara, dan pembatas
+                                    keamanan untuk teller di bank.</p>
                             </article>
                         </div>
                     </div>
                 </div>
             </div>
             <div class="tab-pane fade" id="pills-putih" role="tabpanel" aria-labelledby="pills-putih-tab">...</div>
-            <div class="tab-pane fade" id="pills-transparan" role="tabpanel" aria-labelledby="pills-transparan-tab">...</div>
+            <div class="tab-pane fade" id="pills-transparan" role="tabpanel" aria-labelledby="pills-transparan-tab">...
+            </div>
             <div class="tab-pane fade" id="pills-tinted" role="tabpanel" aria-labelledby="pills-tinted-tab">...</div>
             <div class="tab-pane fade" id="pills-wsb" role="tabpanel" aria-labelledby="pills-wsb-tab">...</div>
             <div class="tab-pane fade" id="pills-neon" role="tabpanel" aria-labelledby="pills-neon-tab">...</div>
-          </div>
+        </div>
     </div>
 </div>
 @stop
