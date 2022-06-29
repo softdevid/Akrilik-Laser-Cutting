717e64988e015831a4fd2e994d1b0d65a113d42c
zionexeonch <zionexeon@gmail.com>
2022-06-29 10:12:39 +0700

    edit code

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,31 +1,3 @@
-<nav class="navbar navbar-expand-lg bg-dark">
-    <div class="container">
-        <ul class="nav justify-content-start align-items-center text-center">
-            <li class="nav-item">
-                <a class="nav-link text-white left-nav" aria-current="page" href="telp:+6285728060268">+62
-                    857-2806-0268</a>
-            </li>
-        </ul>
-
-        <!-- <ul class="nav d-flex">
-            <li class="nav-item">
-                <a class="nav-link text-white active" aria-current="page" href="#"><i class="fa-brands fa-youtube"></i></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link text-white" href="#"><i class="fa-brands fa-facebook"></i></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link text-white" href="#"><i class="fa-brands fa-instagram"></i></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link text-white" href="#"><i class="fa-brands fa-twitter"></i></a>
-            </li>
-        </ul> -->
-    </div>
-</nav>
-
-
-
 <nav class="navbar navbar-expand-lg bg-light sticky-top">
     <div class="container">
         <a class="navbar-brand" href="#">MEKAR LASER <br>
@@ -44,11 +16,11 @@
                         Tentang
                     </a>
                     <ul class="dropdown-menu isi-dropdown" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item {{ ($title === "PROFIL" ) ? 'active' : '' }}" href="/profile">Profil</a></li>
-                        <li><a class="dropdown-item {{ ($title === "TIM" ) ? 'active' : '' }}" href="/team">Tim</a></li>
-                        <li><a class="dropdown-item {{ ($title === "FASE KERJA" ) ? 'active' : '' }}" href="/work-phase">Fase Kerja</a></li>
-                        <li><a class="dropdown-item {{ ($title === "LINGKUP PEKERJAAN" ) ? 'active' : '' }}" href="/scope-of-work">Lingkup Pekerjaan</a></li>
-                        <li><a class="dropdown-item {{ ($title === "RUANG PERTANYAAN" ) ? 'active' : '' }}" href="/faq">Ruang Pertanyaan</a></li>
+                        <li><a class="dropdown-item  {{ ($title === "PROFIL" ) ? 'active' : '' }}" href="/profile">Profil</a></li>
+                        <li><a class="dropdown-item  {{ ($title === "TIM" ) ? 'active' : '' }}" href="/team">Tim</a></li>
+                        <li><a class="dropdown-item  {{ ($title === "FASE KERJA" ) ? 'active' : '' }}" href="/work-phase">Fase Kerja</a></li>
+                        <li><a class="dropdown-item  {{ ($title === "LINGKUP PEKERJAAN" ) ? 'active' : '' }}" href="/scope-of-work">Lingkup Pekerjaan</a></li>
+                        <li><a class="dropdown-item  {{ ($title === "RUANG PERTANYAAN" ) ? 'active' : '' }}" href="/faq">Ruang Pertanyaan</a></li>
                     </ul>
                 </li>
                 <li class="nav-item">
