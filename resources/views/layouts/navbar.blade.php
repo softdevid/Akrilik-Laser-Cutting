de415a3db78ba8a159123888c436aca09d34a6d7
zionexeonch <zionexeon@gmail.com>
2022-06-29 11:54:23 +0700

    edit code

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -9,31 +9,31 @@
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ms-auto">
                 <li class="nav-item">
-                    <a class="nav-link {{ ($title === "BERANDA" ) ? 'active' : '' }}"href="/">Beranda</a>
+                    <a class="nav-link {{ ($title === "BERANDA" ) ? 'active text-danger' : '' }}"href="/">Beranda</a>
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         Tentang
                     </a>
                     <ul class="dropdown-menu isi-dropdown" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item  {{ ($title === "PROFIL" ) ? 'active' : '' }}" href="/profile">Profil</a></li>
-                        <li><a class="dropdown-item  {{ ($title === "TIM" ) ? 'active' : '' }}" href="/team">Tim</a></li>
-                        <li><a class="dropdown-item  {{ ($title === "FASE KERJA" ) ? 'active' : '' }}" href="/work-phase">Fase Kerja</a></li>
-                        <li><a class="dropdown-item  {{ ($title === "LINGKUP PEKERJAAN" ) ? 'active' : '' }}" href="/scope-of-work">Lingkup Pekerjaan</a></li>
-                        <li><a class="dropdown-item  {{ ($title === "RUANG PERTANYAAN" ) ? 'active' : '' }}" href="/faq">Ruang Pertanyaan</a></li>
+                        <li><a class="dropdown-item  {{ ($title === "PROFIL" ) ? 'active bg-dark' : '' }}" href="/profile">Profil</a></li>
+                        <li><a class="dropdown-item  {{ ($title === "TIM" ) ? 'active bg-dark' : '' }}" href="/team">Tim</a></li>
+                        <li><a class="dropdown-item  {{ ($title === "FASE KERJA" ) ? 'active bg-dark' : '' }}" href="/work-phase">Fase Kerja</a></li>
+                        <li><a class="dropdown-item  {{ ($title === "LINGKUP PEKERJAAN" ) ? 'active bg-dark' : '' }}" href="/scope-of-work">Lingkup Pekerjaan</a></li>
+                        <li><a class="dropdown-item  {{ ($title === "RUANG PERTANYAAN" ) ? 'active bg-dark' : '' }}" href="/faq">Ruang Pertanyaan</a></li>
                     </ul>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link {{ ($title === "PRODUK" ) ? 'active' : '' }}" href="/product">Produk</a>
+                    <a class="nav-link {{ ($title === "PRODUK" ) ? 'active text-danger' : '' }}" href="/product">Produk</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link {{ ($title === "LAYANAN" ) ? 'active' : '' }}" href="/services">Layanan</a>
+                    <a class="nav-link {{ ($title === "LAYANAN" ) ? 'active text-danger' : '' }}" href="/services">Layanan</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link {{ ($title === "KONTAK" ) ? 'active' : '' }}" href="/talk">Kontak</a>
+                    <a class="nav-link {{ ($title === "KONTAK" ) ? 'active text-danger' : '' }}" href="/talk">Kontak</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link {{ ($title === "HASIL KERJA" ) ? 'active' : '' }}" href="/portfolio">Hasil Kerja</a>
+                    <a class="nav-link {{ ($title === "HASIL KERJA" ) ? 'active text-danger' : '' }}" href="/portfolio">Hasil Kerja</a>
                 </li>
                 {{-- <li class="nav-item">
                     <a class="nav-link" href="/blog"></a>
